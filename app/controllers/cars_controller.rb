7b2fe0e336ba5e7a748323732f3b0ea061c4ea5f
Eduardo Prauze <eduardoprauze@hotmail.com>
2017-04-07 19:15:57 -0300

    Added cache expiration

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,6 @@
 class CarsController < ApplicationController
   def index
-    @cars = Rails.cache.fetch("cars", expires_in: 10.seconds) do
+    @cars = Rails.cache.fetch("cars", expires_in: 1.minute) do
       @cars = Car.slow_query
     end
     render json: @cars
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,9 +1,15 @@
 class Car < ApplicationRecord
+  after_save :clear_cache
 
   def self.slow_query
     # Simulation of a slow query
     result = ActiveRecord::Base.connection.execute "select pg_sleep(2)"
     all
   end
-  
+
+  def clear_cache
+    p "Cache cleared!"
+    Rails.cache.delete("cars")
+  end
+
 end
